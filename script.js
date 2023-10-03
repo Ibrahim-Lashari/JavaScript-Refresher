049e1e02ccd27080e8638e5438a0259c8a446f21
Ibrahim-Lashari <524Ilashari@frhsd.com>
2023-10-03 13:04:53 -0400

    added javascript

---
+++ script.js
@@ -0,0 +1,33 @@
+let prevVal = ''
+let newVal = ''
+let resultVal = ''
+let mathOperator = ''
+// Store whether decimal or not decimal was clicked
+// (only allow one decimal per value)
+let decimalClicked = false
+// Hold values we want stored in memory
+let valMemStored = ''
+
+function numButPress(num) {
+
+}
+
+function mathButPress(operator) {
+
+}
+
+function equalButPress() {
+
+}
+
+function clearButPress() {
+
+}
+
+function copyButPress() {
+
+}
+
+function pasteButPress() {
+    
+}
